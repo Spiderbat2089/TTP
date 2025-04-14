1a2a0c64b151e9da6faf8d568e1e7eb9d2ff118b
Afloppy <Afloppypancake@icloud.com>
2025-04-13 20:18:06 -0400

    Added way more products

--- backend/src/index.js
+++ backend/src/index.js
@@ -154,6 +154,78 @@ const listings = [
         price: 150,
         description: "Found by the old mill. Looks cool, but tells me to commit crimes. Selling for safety.",
        image: "../../frontend/Images/product4.JPG"
+    },
+    {
+        title: "Axe",
+        price: 0,
+        description: "I've got like 50 of these, so please come and take them off my hands! Please",
+       image: "../../frontend/Images/product5.JPG"
+    },
+    {
+        title: "Lumber",
+        price: 2,
+        description: "MASSIVE MASSIVE sale on this lumber.  You would be stupid not to take up this offer!",
+       image: "../../frontend/Images/product6.JPG"
+    },
+    {
+        title: "Chair",
+        price: 12,
+        description: "Chair chair, chair chair chair chair.  Do you really need more than that?",
+       image: "../../frontend/Images/product7.JPG"
+    },
+    {
+        title: "Chest",
+        price: 45,
+        description: "The loot-box of our time! Who knows what you might get?  You could get a lot of gold, or cool weapons, and you could even get human remains!",
+       image: "../../frontend/Images/product8.JPG"
+    },
+    {
+        title: "Lamp Post",
+        price: 75,
+        description: "NOT STOLEN, No questions and serious buyers please",
+       image: "../../frontend/Images/product9.JPG"
+    },
+    {
+        title: "Cursed Statue",
+        price: 40,
+        description: "This thing is scary, please someome take it off of my hands.  I think it keeps telling me to kill the king.",
+       image: "../../frontend/Images/product10.JPG"
+    },
+    {
+        title: "Bucket",
+        price: 8,
+        description: "Can be used for multiple things like filling it up with water.  I can't think of anymore, but you get the idea.",
+       image: "../../frontend/Images/product11.JPG"
+    },
+    {
+        title: "Ugly Plant",
+        price: 4,
+        description: "My wife says she loves it, but I think it is ugly.  So I decided to give it away.",
+       image: "../../frontend/Images/product12.JPG"
+    },
+    {
+        title: "Forge",
+        price: 263,
+        description: "My old forge, helped me make a lot of things.  I'm retiring so I an giving it away.  Good luck with moving it out.",
+       image: "../../frontend/Images/product13.JPG"
+    },
+    {
+        title: "The Kings Armory",
+        price: 125,
+        description: "shhhh, don't tell anyone I'm giving this out.",
+       image: "../../frontend/Images/product14.JPG"
+    },
+    {
+        title: "Box",
+        price: 7.5,
+        description: "One box, good for storing things.",
+       image: "../../frontend/Images/product15.JPG"
+    },
+    {
+        title: "Awesome Sword",
+        price: 80,
+        description: "I'm going to hate seeing this go, but I do have to give away everything I own.",
+       image: "../../frontend/Images/product16.JPG"
     }
 ];
 
